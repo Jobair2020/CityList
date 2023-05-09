d8197a9437fec86c1823bb61077d8c7d9a8adf42
Jobair2020 <jobairnahian2017@gmail.com>
2023-05-09 23:05:46 +0600

    assignment on ui test is done

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -76,4 +76,23 @@ public class MainActivityTest {
         Espresso.pressBack(); //Back button
     }
 
+    @Test
+    public void switchActivity(){
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton")); //Type a city name
+//        Espresso.pressBack(); //if the keyboard don't minimizes
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+        //Add another city to the list
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Toronto")); //Type a city name
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+        onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
+
+        onView(withId(R.id.showActivity)).check(matches(isDisplayed()));
+        onView(withText("Edmonton")).check(matches(isDisplayed()));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.mainActivity)).check(matches(isDisplayed()));
+    }
+
+
 }
